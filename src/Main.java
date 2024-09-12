b8a08903bba80fcf837a3f2c4140abcd79d88812
Mark Barnes <barnemar002@warren.k12.in.us>
2024-09-12 13:56:50 -0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+        System.out.println(myCar.getMake());
+        myCar.setMake("Chevy");
+        System.out.println(myCar.getMake());
+    }
+}
